aa58b338dc9a7d47030714bd4111f7bdee003023
Alexandre Arturi <alexandrea8@nycstudents.net>
2022-11-15 12:19:33 -0500

    11/15

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args)
+    {
+        int result = 0;
+        int m = 0;
+        while (m < 3) {
+            int k = 0;
+            while (k < 4) {
+                result++;
+                k++;
+                System.out.print("k: " + k);
+                System.out.println(", m: " + m);
+            }
+            m++;
+        }
+        System.out.println("result: " + result);
+
+
+
+
+
+
+    }
 }
